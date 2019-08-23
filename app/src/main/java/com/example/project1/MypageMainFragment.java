262c3b21e69f7ac470d842fa82401437b29c8274
MinJi <choreania@gmail.com>
2019-08-23 22:53:50 +0900

    일단 데이터베이스에 저장까지는 가능 checked 유지 안됨

--- app/src/main/java/com/example/project1/MypageMainFragment.java
+++ app/src/main/java/com/example/project1/MypageMainFragment.java
@@ -26,6 +26,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -35,6 +41,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MypageMainFragment extends Fragment {
+
     @Nullable
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,17 +50,21 @@ public class MypageMainFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = database.getReferenceFromUrl("https://project1-cecd8.firebaseio.com/");
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) { //로그인 되어있을 경우
             View view = inflater.inflate(R.layout.activity_mypage, container, false);
             String email = user.getEmail(); //get user email
+            // Bundle bundle = getArguments();
+
 
             ListView list;
             ArrayList<String> items;
             list = view.findViewById(R.id.list);
             items = new ArrayList<String>();
             items.add("User Email: "+email);
-            items.add("Types of Veganism");
+            items.add("Types of Veganism" );
             items.add("Personal Information");
             items.add("Logout");
 
@@ -71,17 +82,38 @@ public class MypageMainFragment extends Fragment {
 
                         case 1: //채식타입
                             Toast.makeText(getContext(), items.get(position)+"", Toast.LENGTH_SHORT).show();
+
+                            Intent intent1 = new Intent(getActivity(), SelectVeganism.class);
+                            startActivity(intent1);
+                            /*FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference dbRef = database.getReferenceFromUrl("https://project1-cecd8.firebaseio.com/");
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            String uid = user.getUid();
+                            dbRef = database.getReference("/User");
+
+                            dbRef.child("Personal Info").child("Veganism Type").addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                   String veganType = (String)dataSnapshot.child("Veganism Type").getValue(String.class);
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });*/
                             break;
 
                         case 2: //개인정보
                             Toast.makeText(getContext(), items.get(position)+"", Toast.LENGTH_SHORT).show();
+
                             break;
 
                         case 3: //로그아웃
                             FirebaseAuth.getInstance().signOut();
                             Toast.makeText(getContext(), "로그아웃 되었습니다."+"", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getActivity(), BottomActivity.class);
-                            startActivity(intent);
+                            Intent intent2 = new Intent(getActivity(), BottomActivity.class);
+                            startActivity(intent2);
                             break;
                     }
                 }
---
+++ app/src/main/java/com/example/project1/SelectVeganism.java
@@ -0,0 +1,112 @@
+package com.example.project1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SelectVeganism extends AppCompatActivity{
+    public RadioGroup radioGroup;
+    Button submit, clear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_veganism);
+        SharedPreferences pref = getSharedPreferences("preferencesName", MODE_PRIVATE);
+        final SharedPreferences.Editor prefEdit = pref.edit();
+        submit = (Button) findViewById(R.id.submit);
+        clear = (Button) findViewById(R.id.clear);
+        radioGroup = (RadioGroup) findViewById(R.id.groupradio);
+
+        // Uncheck or reset the radio buttons initially
+       // radioGroup.clearCheck();
+
+        // Add the Listener to the RadioGroup
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup
+                        .OnCheckedChangeListener() {
+                    @Override
+
+                    public void onCheckedChanged(RadioGroup group,
+                                                 int checkedId) {
+
+                        // Get the selected Radio Button
+                        RadioButton
+                                radioButton
+                                = (RadioButton) group
+                                .findViewById(checkedId);
+
+                    }
+                });
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference dbRef = database.getReferenceFromUrl("https://project1-cecd8.firebaseio.com/");
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    String uid = user.getUid();
+                    dbRef = database.getReference("/User");
+                    int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                    if (selectedId == -1) {
+                        Toast.makeText(SelectVeganism.this,
+                                "Please select one",
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    } else {
+
+                        RadioButton radioButton
+                                = (RadioButton) radioGroup
+                                .findViewById(selectedId);
+                        radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                if (isChecked)
+                                {radioButton.setChecked(true);}
+                                else
+                                {radioButton.setChecked(false);}
+                                //prefEdit.putBoolean("booleanValue", true).commit();
+                            }
+                        });
+
+                        Toast.makeText(SelectVeganism.this,
+                                radioButton.getText(),
+                                Toast.LENGTH_SHORT)
+                                .show();
+                        dbRef.child(uid).child("Personal Info").child("Veganism Type").setValue(radioButton.getText().toString());
+                        Intent i = new Intent(SelectVeganism.this, BottomActivity.class);
+                        i.putExtra("VeganType", radioButton.getText().toString());
+                        startActivity(i);
+
+
+                    }
+                }
+            }
+        });
+/*
+        clear.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                radioGroup.clearCheck();
+            }
+        });*/
+    }
+}
