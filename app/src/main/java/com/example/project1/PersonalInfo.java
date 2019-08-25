58bed9ca7228698ca1e7f4d32bb7fc111d46a714
kryoustar@gmail.com <kryoustar@gmail.com>
2019-08-25 16:40:38 +0900

    idk :((

--- app/src/main/java/com/example/project1/PersonalInfo.java
+++ app/src/main/java/com/example/project1/PersonalInfo.java
@@ -1,8 +1,9 @@
 package com.example.project1;
 import android.content.Intent;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,11 +11,16 @@ import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PersonalInfo extends AppCompatActivity {
     public EditText nicknameTV, ageTV;
