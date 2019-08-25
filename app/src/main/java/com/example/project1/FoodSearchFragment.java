ecca228d70aef65a3baaf651108ee12a4fe6a566
kryoustar@gmail.com <kryoustar@gmail.com>
2019-08-25 21:39:44 +0900

    tried FoodSearchFragment :(

--- app/src/main/java/com/example/project1/FoodSearchFragment.java
+++ app/src/main/java/com/example/project1/FoodSearchFragment.java
@@ -1,10 +1,13 @@
 package com.example.project1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,44 +16,57 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class FoodSearch extends AppCompatActivity implements View.OnClickListener {
+public class FoodSearchFragment extends Fragment {
     Button searchButton;
     EditText searchText;
 
-
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.food_search);
 
-        searchText = findViewById(R.id.search_nutrients);
-        searchButton = findViewById(R.id.add_btn);
-        searchButton.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        Intent intent = getIntent();
-        String date = intent.getStringExtra("Date");
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.food_search, container, false);
+        boolean isBreakfast, isLunch, isDinner, isSnack;
+        //Intent intent = getIntent();
+       // String date = intent.getStringExtra("Date");
+        String date = "tryTemp";// getArguments().getString("Date");
 
         Bundle bundle = new Bundle();
         FoodListFragment foodListFragment = new FoodListFragment();
 
-        boolean isBreakfast = (boolean) intent.getBooleanExtra("isBreakfast", false);
-        boolean isLunch = (boolean) intent.getBooleanExtra("isLunch", false);
-        boolean isDinner = (boolean) intent.getBooleanExtra("isDinner", false);
-        boolean isSnack = (boolean) intent.getBooleanExtra("isSnack", false);
-        //DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-        //DatabaseReference conditionRef = mRootRef.child("UserId");
+        EditText searchText = view.findViewById(R.id.search_nutrients);
+        Button searchButton = view.findViewById(R.id.add_btn);
+        //searchButton.setOnClickListener(getView());
+        Bundle args = getArguments();
+        if(args ==null){
+            Toast.makeText(getActivity(), "arguments is null " , Toast.LENGTH_LONG).show();
+            isBreakfast = false;
+            isLunch = true;
+            isDinner = false;
+            isSnack = false;
+        }
+        else {
+             isBreakfast = (boolean) getArguments().getBoolean("isBreakfast");
+             isLunch = (boolean) getArguments().getBoolean("isLunch");
+             isDinner = (boolean) getArguments().getBoolean("isDinner");
+             isSnack = (boolean) getArguments().getBoolean("isSnack");
+        }
         TextView breakfastView;
-
         String search = searchText.getText().toString(); //검색 받아옴
         String sql;
         SQLiteDatabase db;   // db를 다루기 위한 SQLiteDatabase 객체 생성
@@ -58,9 +74,8 @@ public class FoodSearch extends AppCompatActivity implements View.OnClickListene
         ListView listView;   // ListView 객체 생성
         String[] result;   // ArrayAdapter에 넣을 배열 생성
         Integer[] foodnumber;
-
-        db = openOrCreateDatabase("nutrients.db", MODE_PRIVATE, null);
-        listView = (ListView) findViewById(R.id.listView);
+        db = getContext().openOrCreateDatabase("nutrients.db", Context.MODE_PRIVATE, null);
+        listView = (ListView) view.findViewById(R.id.listView);
 
         sql = "select * from tb_nutrients where name like '%" + search + "%'";
         c = db.rawQuery(sql, null);   // select 사용시 사용(sql문, where조건 줬을 때 넣는 값)
@@ -95,41 +110,63 @@ public class FoodSearch extends AppCompatActivity implements View.OnClickListene
                     boolean BreakfastFlag = false;
                     bundle.putBoolean("BreakfastFlag", BreakfastFlag);
                     foodListFragment.setArguments(bundle);
-
-                    onBackPressed();
+                    ((BottomActivity) getActivity()).replaceFragment(FoodListFragment.newInstance(bundle));
+                   // onBackPressed();
                 }
                 if (isLunch) {
                     DatabaseManager.LunchDataAdd(date, foodnumber[position]);
                     boolean LunchFlag = false;
                     bundle.putBoolean("LunchFlag", LunchFlag);
                     foodListFragment.setArguments(bundle);
+                    ((BottomActivity) getActivity()).replaceFragment(FoodListFragment.newInstance(bundle));
 
-                    onBackPressed();
+                    // onBackPressed();
                 }
                 if (isDinner) {
                     DatabaseManager.DinnerDataAdd(date, foodnumber[position]);
                     boolean DinnerFlag = false;
                     bundle.putBoolean("DinnerFlag", DinnerFlag);
                     foodListFragment.setArguments(bundle);
+                    ((BottomActivity) getActivity()).replaceFragment(FoodListFragment.newInstance(bundle));
 
-                    onBackPressed();
+                    //BottomActivity.replaceFragment(FoodListFragment.newInstance());
+                   // onBackPressed();
                 }
                 if (isSnack) {
                     DatabaseManager.SnackDataAdd(date, foodnumber[position]);
                     boolean SnackFlag = false;
                     bundle.putBoolean("SnackFlag", SnackFlag);
                     foodListFragment.setArguments(bundle);
+                    ((BottomActivity) getActivity()).replaceFragment(FoodListFragment.newInstance(bundle));
 
-                    onBackPressed();
+
+                    //  onBackPressed();
                 } /*else { //error ?
 
                     onBackPressed();
                 }*/
             }
         });
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, result);   // ArrayAdapter(this, 출력모양, 배열)
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, result);   // ArrayAdapter(this, 출력모양, 배열)
         listView.setAdapter(adapter);   // listView 객체에 Adapter를 붙인다
+
+
+
+
+
+        return view;
     }
 
-}
 
+    public static FoodSearchFragment newInstance(Bundle bundle){
+        FoodSearchFragment fragment = new FoodSearchFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+  /*  public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment).commit();
+    }*/
+
+}
